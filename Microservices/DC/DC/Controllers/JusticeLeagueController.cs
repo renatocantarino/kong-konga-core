b74435d0d1247fe6e690575122c5bd792028e904
Renato Cantarino <cast.carlosc.utic@sebrae.com.br>
2021-03-17 19:25:28 -0300

    inital

---
+++ Microservices/DC/DC/Controllers/JusticeLeagueController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DC.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class JusticeLeagueController : Controller
+    {
+        [HttpGet]
+        public string Strongest()
+        {
+            return "Batman!";
+        }
+    }
+}
